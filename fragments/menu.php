63acc754a9e8ee5ba759185818836c02baf6317e
Filip Pie <filipiusz@gmail.com>
2019-12-19 16:55:21 +0100

    project folder name detection added to menu.php

--- fragments/menu.php
+++ fragments/menu.php
@@ -2,15 +2,17 @@
 $serverName = $_SERVER['SERVER_NAME'];
 $uri = $_SERVER['REQUEST_URI'];
 $base = $serverName . $uri;
-$end = strpos($base, "photoUploadGallery");
-$webRoot = "http://".(substr($base, 0, $end)) . "photoUploadGallery/";
+$menuPathArray = explode("/", __FILE__);
+$projectFolderName = $menuPathArray[count($menuPathArray) - 3];
+$end = strpos($base, $projectFolderName);
+$webRoot = "http://" . (substr($base, 0, $end)) . $projectFolderName . "/";
 define('SITE_URL', $webRoot);
 ?>
 
 
 <nav class="navbar navbar-expand-lg my-0 py-0 navbar-dark bg-dark">
     <div class="container my-0 py-0">
-        <a class="navbar-brand" id="link1" href="<?= SITE_URL.'index.php' ?>"><i class="fas fa-camera"></i> Photo Upload Gallery</a>
+        <a class="navbar-brand" id="link1" href="<?= SITE_URL . 'index.php' ?>"><i class="fas fa-camera"></i> Photo Upload Gallery</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -22,15 +24,15 @@ define('SITE_URL', $webRoot);
                     </a>
                     <!-- Here's the magic. Add the .animate and .slide-in classes to your .dropdown-menu and you're all set! -->
                     <div class="dropdown-menu dropdown-menu-right animate slideIn" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" id="link2" href="<?= SITE_URL.'index.php' ?>">Homepage</a>
+                        <a class="dropdown-item" id="link2" href="<?= SITE_URL . 'index.php' ?>">Homepage</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" id="link3" href="<?= SITE_URL.'views/thumbnailGallery.php' ?>">Gallery</a>
-                        <a class="dropdown-item" id="link4" href="<?= SITE_URL.'views/show.php' ?>">Slider</a>
-                        <a class="dropdown-item" id="link5" href="<?= SITE_URL.'views/legalNote.php' ?>">Legal Note</a>
+                        <a class="dropdown-item" id="link3" href="<?= SITE_URL . 'views/thumbnailGallery.php' ?>">Gallery</a>
+                        <a class="dropdown-item" id="link4" href="<?= SITE_URL . 'views/show.php' ?>">Slider</a>
+                        <a class="dropdown-item" id="link5" href="<?= SITE_URL . 'views/legalNote.php' ?>">Legal Note</a>
                     </div>
                 </li>
             </ul>
         </div>
-        
+
     </div>
 </nav>
